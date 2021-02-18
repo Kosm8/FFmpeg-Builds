254c519650e914494284f9aeebc79e8fd3f7cdf6
Kosm8 <77066077+Kosm8@users.noreply.github.com>
2021-02-18 13:23:25 +0100

    Update 50-soxr.sh

--- scripts.d/50-soxr.sh
+++ scripts.d/50-soxr.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 # https://sourceforge.net/p/soxr/code/ci/master/tree/
-SOXR_REPO="https://git.code.sf.net/p/soxr/code"
+# SOXR_REPO="https://git.code.sf.net/p/soxr/code"
+SOXR_REPO="https://github.com/chirlu/soxr.git"
 SOXR_COMMIT="945b592b70470e29f917f4de89b4281fbbd540c0"
 
 ffbuild_enabled() {
