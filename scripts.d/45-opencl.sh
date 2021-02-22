2086fd4f5bbf2a3a374b3b3658705b7c42446391
Kosm8 <77066077+Kosm8@users.noreply.github.com>
2021-02-22 09:22:08 +0100

    Update 45-opencl.sh

--- scripts.d/45-opencl.sh
+++ scripts.d/45-opencl.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 HEADERS_REPO="https://github.com/KhronosGroup/OpenCL-Headers.git"
-HEADERS_COMMIT="23710f1b99186065c1768fc3098ba681adc0f253"
+HEADERS_COMMIT="d1b936b72b9610626ecab8a991cec18348fba047"
 
 LOADER_REPO="https://github.com/KhronosGroup/OpenCL-ICD-Loader.git"
 LOADER_COMMIT="99df8d88f7509739be7849da03a9fb1fb8bcbfa4"
