8eab377c0956b638d0688d63fa290c77fc050ab2
Kosm8 <77066077+Kosm8@users.noreply.github.com>
2021-02-23 16:40:59 +0100

    Update 45-vmaf.sh

--- scripts.d/45-vmaf.sh
+++ scripts.d/45-vmaf.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 VMAF_REPO="https://github.com/Netflix/vmaf.git"
-VMAF_COMMIT="7138a0b8f9834ca33a7712c174cad68dc2b770f8"
+VMAF_COMMIT="bb20d3616f729e69dbc7b61f50a6b9a3903282a0"
 
 ffbuild_enabled() {
     return -1
